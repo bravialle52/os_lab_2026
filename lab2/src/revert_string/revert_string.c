437a767015e1d05aafc0e2ae467dc453e341f519
bravialle52 <osnbravialle@gmail.com>
2026-09-05 14:48:31 +0000

    Выполнил Лабораторную работу 2, но нужно чуть разобраться

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
-	// your code here
-}
+    if (str == NULL) return;
 
+    int len = strlen(str);
+    // Выделяем память под временную копию строки в куче (+1 для '\0')
+    char *temp = (char *)malloc((len + 1) * sizeof(char));
+
+    strcpy(temp, str);
+
+    // Переворачиваем строку, записывая символы с конца temp в начало str
+    for (int i = 0; i < len; i++) {
+        str[i] = temp[len - 1 - i];
+    }
+
+    // Освобождаем выделенную память, чтобы избежать утечек
+    free(temp);
+}
